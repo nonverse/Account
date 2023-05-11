c819ed4bcaaf44d1d0238b0c976e651076b004a7
Isuru <isuru2003a@gmail.com>
2023-05-11 14:49:42 +1000

    Add animation to navigation

---
+++ src/elements/NavLink.jsx
@@ -0,0 +1,39 @@
+import {NavLink as ReactNavLink} from "react-router-dom";
+import {motion} from "framer-motion";
+
+const NavLink = ({to, children}) => {
+
+    const variants = {
+        active: {
+            backgroundColor: "#6951FF",
+            opacity: ".4",
+            borderRadius: "8px",
+            paddingLeft: "1.5rem",
+            marginLeft: "2rem",
+            color: "#ECF0F3",
+        },
+        inactive: {
+            backgroundColor: "transparent",
+            borderRadius: 0,
+            paddingLeft: 0,
+            marginLeft: 0,
+            opacity: 1,
+            color: "#333344",
+        }
+    }
+
+    return (
+        <ReactNavLink className="nav-link" to={to}>
+            {({isActive}) => (
+                <motion.span
+                    animate={isActive ? 'active' : 'inactive'}
+                    variants={variants}
+                >
+                    {children}
+                </motion.span>
+            )}
+        </ReactNavLink>
+    )
+}
+
+export default NavLink
