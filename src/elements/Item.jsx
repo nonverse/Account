ad8cc868117bcddab55bb3cec2218f458250351f
Isuru <isuru2003a@gmail.com>
2023-05-11 19:07:32 +1000

    Allow edit button to be optional, and for custom edit text

--- src/elements/Item.jsx
+++ src/elements/Item.jsx
@@ -1,4 +1,5 @@
-const Item = ({name, value, editable = false, selector, editAction}) => {
+const Item = ({name, value, selector="pencil", action}) => {
+
 
     return (
         <div className="item">
@@ -7,12 +8,20 @@ const Item = ({name, value, editable = false, selector, editAction}) => {
                 <span>{value}</span>
                 <div className="item-line"/>
             </div>
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-                 strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
-                 className="item-edit"
-                 onClick={() => editAction()}>
-                <path d="M17 3a2.828 2.828 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5L17 3z"></path>
-            </svg>
+            {action ? (
+                <>
+                    {(selector === "pencil") ? (
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                             strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
+                             className="item-edit"
+                             onClick={() => action()}>
+                            <path d="M17 3a2.828 2.828 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5L17 3z"></path>
+                        </svg>
+                    ) : (
+                        <span className="item-edit splash">{selector}</span>
+                    )}
+                </>
+            ) : ''}
         </div>
     )
 }
