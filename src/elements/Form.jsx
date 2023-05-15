fcd639c6e395a29c0b09a524166dca8837bb1d98
isuru <isuru2003a@gmail.com>
2023-05-15 20:12:31 +0530

    Add loader to form element

--- src/elements/Form.jsx
+++ src/elements/Form.jsx
@@ -1,13 +1,17 @@
 import {Form as FormikForm} from "formik";
+import Loader from "../components/Loader";
 
-const Form = ({id, children}) => {
+const Form = ({id, loading, children}) => {
 
     return (
-        <FormikForm id={id}>
-            {children}
-            <button type="submit">Done</button>
-        </FormikForm>
+        <div className="form-wrapper">
+            <FormikForm className={`form ${loading ? 'element-disabled' : ''}`} id={id}>
+                {children}
+                <button type="submit">Done</button>
+            </FormikForm>
+            {loading ? <Loader/> : <></>}
+        </div>
     )
 }
 
-export default Form 
+export default Form
