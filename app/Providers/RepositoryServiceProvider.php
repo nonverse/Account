444431dc3233fe649dbc9da81493444ff5e77dd7
Isuru <isuru2003a@gmail.com>
2023-06-06 23:07:24 +1000

    Bind refresh token repository

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -2,8 +2,10 @@
 
 namespace App\Providers;
 
+use App\Contracts\Repository\RefreshTokenRepositoryInterface;
 use App\Contracts\Repository\RepositoryInterface;
 use App\Contracts\Repository\UserRepositoryInterface;
+use App\Repositories\RefreshTokenRepository;
 use App\Repositories\Repository;
 use App\Repositories\UserRepository;
 use Illuminate\Support\ServiceProvider;
@@ -13,5 +15,6 @@ class RepositoryServiceProvider extends ServiceProvider
     public function register()
     {
         $this->app->bind(RepositoryInterface::class, Repository::class);
+        $this->app->bind(RefreshTokenRepositoryInterface::class, RefreshTokenRepository::class);
     }
 }
