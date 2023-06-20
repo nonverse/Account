f8d7b830d8fde25df03064b7a3d76c81a9ccc48b
Isuru <isuru2003a@gmail.com>
2023-06-21 00:47:22 +1000

    Create middleware to attach authorization token to outgoing requests if required

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Http;
 
+use App\Http\Middleware\AddAuthorizationToken;
 use App\Http\Middleware\RefreshAccessToken;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
@@ -41,7 +42,7 @@ class Kernel extends HttpKernel
 
         'api' => [
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-            \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
+            \Illuminate\Routing\Middleware\ThrottleRequests::class . ':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
     ];
@@ -64,6 +65,7 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'token' => RefreshAccessToken::class
+        'token' => RefreshAccessToken::class,
+        'withauthorization' => AddAuthorizationToken::class,
     ];
 }
