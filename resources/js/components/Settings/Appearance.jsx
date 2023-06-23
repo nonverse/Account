a4b320f5974063ee6c57f15407813ed3b1761161
Isuru <isuru2003a@gmail.com>
2023-06-23 21:24:16 +1000

    Create appearance sector

---
+++ resources/js/components/Settings/Appearance.jsx
@@ -0,0 +1,18 @@
+import Sector from "@/components/Sector.jsx";
+import Item from "@/elements/Item.jsx";
+
+const Appearance = () => {
+
+    return (
+        <Sector heading="Appearance">
+            <Item name="Theme" value="Light" action={() => {
+
+            }}/>
+            <Item name="Language" value="English (AU)" action={() => {
+
+            }}/>
+        </Sector>
+    )
+}
+
+export default Appearance
