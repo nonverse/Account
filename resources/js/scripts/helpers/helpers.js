28a97c300c84527f89dcabcd3f01e5862d03e4a3
Isuru <isuru2003a@gmail.com>
2023-06-23 23:05:18 +1000

    Create helper to capitalise first letter of string

--- resources/js/scripts/helpers/helpers.js
+++ resources/js/scripts/helpers/helpers.js
@@ -8,6 +8,12 @@ class helpers {
         const item =  Object.keys(object).find((key) => object[key][attribute] === value)
         return object[item]
     }
+
+    capitaliseFirst(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1)
+    }
+
+
 }
 
 export default new helpers()
