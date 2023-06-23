47ded939029b5fd9a4c0f2cb8f42760024315c49
Isuru <isuru2003a@gmail.com>
2023-06-23 21:24:58 +1000

    Create settings store

--- resources/js/state/store.js
+++ resources/js/state/store.js
@@ -1,12 +1,14 @@
 import {combineReducers, configureStore} from "@reduxjs/toolkit";
 import userReducer from "../state/user";
 import modalReducer from "../state/app/modal"
+import settingsReducer from "../state/app/settings.js"
 
 export default configureStore({
     reducer: {
         user: userReducer,
         application: combineReducers({
             modal: modalReducer,
+            settings: settingsReducer
         })
     },
 })
