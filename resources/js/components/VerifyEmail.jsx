08692c084e1a73a582e5251bbf184c61f8a944ea
Isuru <isuru2003a@gmail.com>
2023-07-02 01:29:52 +1000

    Add logic to post email verification token to API

--- resources/js/components/VerifyEmail.jsx
+++ resources/js/components/VerifyEmail.jsx
@@ -2,22 +2,27 @@ import Loader from "@/components/Loader.jsx";
 import {useEffect} from "react";
 import api from "@/scripts/api.js";
 import {useNavigate} from "react-router";
+import {useDispatch, useSelector} from "react-redux";
+import {updateUser} from "@/state/user.js";
 
 const VerifyEmail = () => {
 
+    const user = useSelector(state => state.user.value)
     const query = new URLSearchParams(window.location.search)
+    const dispatch = useDispatch()
     const navigate = useNavigate()
 
     useEffect(() => {
         async function verify() {
             await api.post('auth/verify-email', {
-                expires: query.get('expires'),
-                hash: query.get('hash'),
-                id: query.get('id'),
-                signature: query.get('signature')
+                token: query.get('token')
             })
                 .then((response) => {
                     if (response.data.success) {
+                        dispatch(updateUser({
+                            ...user,
+                            email_verified_at: 'Just now'
+                        }))
                         navigate('/')
                     }
                 })
