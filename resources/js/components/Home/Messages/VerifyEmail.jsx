a0d7c919fd7e2006d557689c6334c70696c8d3ec
Isuru <isuru2003a@gmail.com>
2023-07-03 23:43:51 +1000

    Create helper method to convert modal into json query

--- resources/js/components/Home/Messages/VerifyEmail.jsx
+++ resources/js/components/Home/Messages/VerifyEmail.jsx
@@ -1,12 +1,13 @@
 import MessageBox from "@/components/MessageBox.jsx";
 import InLineButton from "@/elements/InLineButton.jsx";
+import helpers from "@/scripts/helpers/helpers.js";
 
 const VerifyEmail = () => {
 
     return (
         <MessageBox id="verify-email" weight="danger">
             Your e-mail is not verified. <InLineButton onClick={() => {
-            window.location = '/personal-info?state={"modal":{"value":{"id":"update_email"}}}'
+            window.location = `/personal-info?state=${helpers.addModalToQuery('update_email')}`
         }}>Click here</InLineButton> to change your e-mail or re-send the verification
         </MessageBox>
     )
--- resources/js/scripts/helpers/helpers.js
+++ resources/js/scripts/helpers/helpers.js
@@ -5,7 +5,7 @@ class helpers {
     }
 
     getObjectItem(object, attribute, value) {
-        const item =  Object.keys(object).find((key) => object[key][attribute] === value)
+        const item = Object.keys(object).find((key) => object[key][attribute] === value)
         return object[item]
     }
 
@@ -13,7 +13,15 @@ class helpers {
         return string.charAt(0).toUpperCase() + string.slice(1)
     }
 
-
+    addModalToQuery(modalId) {
+        return JSON.stringify({
+            modal: {
+                value: {
+                    id: modalId
+                }
+            }
+        })
+    }
 }
 
 export default new helpers()
