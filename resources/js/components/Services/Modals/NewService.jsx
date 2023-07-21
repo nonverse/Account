f2ebe2d9c5085c1eafa636565984d36bf9a8b9c8
Isuru <isuru2003a@gmail.com>
2023-07-21 21:54:39 +1000

    Add service modal navigation functions

--- resources/js/components/Services/Modals/NewService.jsx
+++ resources/js/components/Services/Modals/NewService.jsx
@@ -1,13 +1,23 @@
 import ScreenModal from "@/components/ScreenModal.jsx";
 import {useState} from "react";
 import SelectService from "@/components/Services/Modals/SelectService.jsx";
+import Minecraft from "@/components/Services/Modals/Services/Minecraft.jsx";
 
 const NewService = () => {
 
     const [state, setState] = useState(0)
     const [loading, setLoading] = useState(false)
     const views = {
-        0: <SelectService/>
+        0: <SelectService toService={toService}/>,
+        1: <Minecraft back={back}/>
+    }
+
+    function toService(service) {
+        setState(service)
+    }
+
+    function back() {
+        setState(0)
     }
 
     return (
--- resources/js/components/Services/Modals/SelectService.jsx
+++ resources/js/components/Services/Modals/SelectService.jsx
@@ -1,15 +1,17 @@
 import Card from "@/elements/Card.jsx";
 import minecraft from "../../../../assets/minecraft.svg"
 
-const SelectService = () => {
+const SelectService = ({toService}) => {
 
     return (
         <>
             <p id="new-service-text">
-                Select the service that you wish to connect to your Nonverse account. All services listed below are built and trusted by Nonverse Studios (That's us)
+                Select the service that you wish to connect to your Nonverse account. All services listed below are built and/or trusted by Nonverse Studios (That's us)
             </p>
             <div id="services">
-                <Card noDisplayName name="Minecraft" icon={minecraft} value="Nonverse official Minecraft server"/>
+                <Card noDisplayName name="Minecraft" icon={minecraft} value="Nonverse official Minecraft server" onClick={() => {
+                    toService(1)
+                }}/>
             </div>
         </>
     )
