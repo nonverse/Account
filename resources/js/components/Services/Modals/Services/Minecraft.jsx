fce3646ed1e6aca8e486bb92edc5e6b1638661f8
Isuru <isuru2003a@gmail.com>
2023-08-01 21:58:04 +1000

    Move to submodal component

--- resources/js/components/Services/Modals/Services/Minecraft.jsx
+++ resources/js/components/Services/Modals/Services/Minecraft.jsx
@@ -1,58 +1,22 @@
-import {Formik} from "formik";
-import Form from "@/elements/Form.jsx";
-import Field from "@/elements/Field.jsx";
-import validate from "@/scripts/validate.js";
-import api from "@/scripts/api.js";
 import {useState} from "react";
+import MinecraftUsername from "@/components/Services/Modals/Services/MinecraftUsername.jsx";
+import VerifyMinecraftProfile from "@/components/Services/Modals/Services/VerifyMinecraftProfile.jsx";
 
-const Minecraft = ({back}) => {
+const Minecraft = () => {
 
-    const [error, setError] = useState('')
-    const [loading, setLoading] = useState(false)
+    const [state, setState] = useState(0)
+    const views = {
+        0: <MinecraftUsername advance={advance}/>,
+        1: <VerifyMinecraftProfile/>
+    }
 
-    function validateUsername(value) {
-        setError('')
-        return validate.require(value)
+    function advance() {
+        setState(state + 1)
     }
 
     return (
         <>
-            <Formik initialValues={{
-                username: ''
-            }} onSubmit={async (values) => {
-                setLoading(true)
-                await api.post('user/services/minecraft/validate', values)
-                    .then(response => {
-                        //
-                    })
-                    .catch(e => {
-                        switch (e.response.status) {
-                            case 400:
-                                setError('Profile not found')
-                                break
-                            default:
-                                setError('Something went wrong')
-                                break
-                        }
-                        setLoading(false)
-                    })
-            }}>
-                {({errors}) => (
-                    <Form id="screen-modal-form" cta="Continue" loading={loading}>
-                        <Field name="username" label="Minecraft username" validate={validateUsername}
-                               error={errors.username ? errors.username : error}/>
-                        <div id="screen-modal-text">
-                            <p>
-                                You must have a valid and genuine Minecraft license in order to connect your profile to
-                                your account
-                                <br/>
-                                Nonverse Studios is not affiliated with Mojang or Microsoft in any way.
-                            </p>
-                        </div>
-                    </Form>
-                )}
-            </Formik>
-
+            {views[state]}
         </>
     )
 }
